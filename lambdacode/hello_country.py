595afc57152b3b06b0a74a8c58146849ba1eac92
Warren Wei <warrenwei.work@gmail.com>
2021-05-21 01:24:00 +1000

    Add some testing code

---
+++ lambdacode/hello_country.py
@@ -0,0 +1,15 @@
+import json
+import datetime
+import requests
+def lambda_handler(event, context):
+  print ('event: ')
+  print (event)
+  data = {
+    'output': 'Hello from ' + event['Country'],
+    'timestamp': datetime.datetime.utcnow().isoformat()
+  }
+  return {
+    'statusCode:': 200,
+    'body': json.dumps(data),
+    'headers': {'Content-Type': 'application/json'}
+  }
